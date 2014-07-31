6409b3febd4b09128b481199103de871f87b3d49
Juan Huh <juan.huh@hotmail.com>
2014-07-31 12:03:29 +0000

    test191

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,14 +36,6 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
   config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.default_url_options = { host: 'localhost:3000' }
-  
-  config.paperclip_defaults = {
-storage: :s3,
-  s3_credentials: {
-    bucket: ENV['AWS_BUCKET'],
-    access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-    secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
-  }
-}
+
 
 end
